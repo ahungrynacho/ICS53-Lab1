1784f2ee31a2fbba6cf46930802d742bbf13e591
ahungrynacho <brianbhuynh@gmail.com>
2016-03-29 18:47:23 -0700

    updated

--- lab1.c
+++ lab1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
+// does dynamic allocation need to be supported?
 //struct Person {
 	//char* name = calloc(100, sizeof(char));
 	//char* addr = calloc(100, sizeof(char));
@@ -12,19 +13,43 @@
 	//Person* person = calloc(10, sizeof(Person));
 //}
 
-void read(const char* file_name) {
+typedef struct ListNode{
+	struct ListNode* next;
+	int id;
+	char* str;
+} LN;
+
+char* read(const char* file_name) {
 	FILE* infile = fopen(file_name, "r");
-	int c;
-	while ((c = getc(infile)) != EOF) {
-		putchar(c);
-	}
+	static char fileArray[512];
+	fread(fileArray, sizeof(fileArray), 1, infile);
+	
+	const char delim[2] = "\n";
+	char* line;
+	line = strtok(fileArray, delim);
+	
+	LN* node = NULL;
+	node = malloc(sizeof(LN));
+	node->id = 0;
+	node->next = NULL;
+
+	//okay
+	//new commit
 	fclose(infile);
+	return fileArray;
 }
 
 void write() {
 }
 
 int main(){
-	read("record.txt");
+	char* fileArray = read("record.txt");
+	//int i;
+	
+	//for (i = 0; fileArray[i] != '\0'; ++i) {
+		//putchar(fileArray[i]);
+	//}
+	
 	return 0;
 }
+
