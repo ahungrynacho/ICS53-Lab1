ce9cd252bcea71b7b9a9ef57be0f08017d4eb352
ahungrynacho <brianbhuynh@gmail.com>
2016-04-01 00:23:09 -0700

    Begin conversion from C++ to C

--- lab1.c
+++ lab1.c
@@ -1,55 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-// does dynamic allocation need to be supported?
-//struct Person {
-	//char* name = calloc(100, sizeof(char));
-	//char* addr = calloc(100, sizeof(char));
-	//char* phone = calloc(100, sizeof(char));
-//};
-
-//struct Record {
-	//int total = 0;
-	//Person* person = calloc(10, sizeof(Person));
-//}
 
 typedef struct ListNode{
+	
+	char* name;
+	char* address;
+	char* phone;
 	struct ListNode* next;
-	int id;
-	char* str;
 } LN;
 
-char* read(const char* file_name) {
-	FILE* infile = fopen(file_name, "r");
-	static char fileArray[512];
-	fread(fileArray, sizeof(fileArray), 1, infile);
+LN* read(const char* file){
+	LN* head = malloc(sizeof(LN));
+	LN* tail = malloc(sizeof(LN));
+	LN* front = malloc(sizeof(LN));
 	
-	const char delim[2] = "\n";
-	char* line;
-	line = strtok(fileArray, delim);
+	char line[1023];
+	FILE* fp = fopen(file, "r");
+	while (fgets(line, 1023, fp) != NULL) { //NULL == EOF
+		char* info = malloc
+		char* token = strtok(line, "\t");
+		while (token != NULL) {
+			printf("%s", token);
+			token = strtok(NULL, "\t");
+		}
+	}
+	return head;
 	
-	LN* node = NULL;
-	node = malloc(sizeof(LN));
-	node->id = 0;
-	node->next = NULL;
-
-	//okay
-	//new commit
-	fclose(infile);
-	return fileArray;
 }
 
-void write() {
+void write(LN* node, const char* file) {
+}
+
+//~ void remove(LN* node, int id) {
+//~ }
+
+void deleteList(LN* node) {
 }
 
 int main(){
-	char* fileArray = read("record.txt");
-	//int i;
-	
-	//for (i = 0; fileArray[i] != '\0'; ++i) {
-		//putchar(fileArray[i]);
-	//}
-	
+	read("record.txt");
+	//~ char* str1 = "abc";
+	//~ str1 = "123";
+	//~ puts(str1);
 	return 0;
 }
 
