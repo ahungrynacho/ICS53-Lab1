5ae7c615dafdb014853a54eaa1d213e6030b8ea8
ahungrynacho <brianbhuynh@gmail.com>
2016-04-01 13:30:40 -0700

    Debugging Linked List

--- lab1.c
+++ lab1.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 
 typedef struct ListNode{
-	
 	char* name;
 	char* address;
 	char* phone;
@@ -12,19 +11,41 @@ typedef struct ListNode{
 
 LN* read(const char* file){
 	LN* head = malloc(sizeof(LN));
-	LN* tail = malloc(sizeof(LN));
-	LN* front = malloc(sizeof(LN));
+	//~ LN* tail = malloc(sizeof(LN));
+	//~ head->next = tail;
+	//~ tail->next = NULL;
+	//~ LN* front = head;
 	
 	char line[1023];
 	FILE* fp = fopen(file, "r");
 	while (fgets(line, 1023, fp) != NULL) { //NULL == EOF
-		char* info = malloc
-		char* token = strtok(line, "\t");
+		char* infoList[4]; //[name, address, phone]
+		int tokenPos = 0;
+		char* token = strtok(line, "\t"); 
+		
 		while (token != NULL) {
-			printf("%s", token);
+			infoList[tokenPos] = token;
+			tokenPos++;
 			token = strtok(NULL, "\t");
 		}
+		
+		LN* newNode = malloc(sizeof(LN));
+		newNode->name = infoList[0];
+		newNode->address = infoList[1];
+		newNode->phone = infoList[2];
+
+		newNode->next = head;
+		head = newNode;
+		//~ head = head->next = newNode;
+		//~ newNode->next = tail;
+		//~ printf("%s\t%s\t%s", head->name, head->address, head->phone);
 	}
+	//printf("%s\t%s\t%s", front->next->name, front->next->address, front->next->phone);
+	//printf("%s\t%s\t%s", head->name, head->address, head->phone);
+	//front = front->next;
+	//printf("%s\t%s\t%s", front->name, front->address, front->phone);
+
+
 	return head;
 	
 }
@@ -32,14 +53,24 @@ LN* read(const char* file){
 void write(LN* node, const char* file) {
 }
 
-//~ void remove(LN* node, int id) {
-//~ }
+void remove_node(LN* node, int id) {
+}
 
-void deleteList(LN* node) {
+void print(LN* node) {
+	int count = 1;
+	while (node->next != NULL) {
+		printf("%i %s\t%s\t%s", count, node->name, node->address, node->phone);
+		node = node->next;
+		count++;
+	}
+}
+
+void delete_list(LN* node) {
 }
 
 int main(){
-	read("record.txt");
+	LN* database = read("record.txt");
+	print(database);
 	//~ char* str1 = "abc";
 	//~ str1 = "123";
 	//~ puts(str1);
